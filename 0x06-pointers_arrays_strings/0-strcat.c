584ad47acd9264479f59394e702d12bf28e4ddcf
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-09-20 13:29:25 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,10 +13,9 @@ char *_strcat(char *dest, char *src)
 	int myResults = strlen(dest);
 
 	i = 0;
-	while (src[i] != '\0')
-	{
-		dest(myResults + i)  = src[i];
-		dest(myResults + i) = '\0';
-	}
+	for (i = 0; src[i] != '\0'; i++)
+		dest[myResults + i] = src[i];
+
+	dest[myResults + i] = '\0';
 	return (dest);
 }
