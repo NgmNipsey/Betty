323db1293d52967839857a6886362534d99ac4b4
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-09-25 14:01:45 -0700

    Pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - pointer to a pointer.
+ * @s: store pointer to a pointer.
+ * @to: store pointer.
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
