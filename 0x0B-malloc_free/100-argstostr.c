3a85c4a32920e2b38ff46d3c90cf0b9ce0125ccd
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-04 18:06:41 -0700

    Concatenates all arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all arguments.
+ * @ac: string.
+ * @av: Pointer string.
+ * Return: Pointer to main.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+	len++;
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
