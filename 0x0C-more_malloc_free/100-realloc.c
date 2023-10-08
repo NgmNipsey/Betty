db2a3a519f6ffe4e6e7abc9522eab4ffbec5e7be
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-08 15:45:46 -0700

    reallocate memory blocks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	min_size = old_size < new_size? old_size : new_size;
+	min_size = old_size < new_size ? old_size : new_size;
 	for (i = 0; i < min_size; i++)
 	{
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
