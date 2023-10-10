d98eb68ea721318c72b33147ba1fd3ce6c36fc32
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-10 16:25:53 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	dog->name = _strcpy(dog->name, name);
-	dog->age = name;
+	dog->age = age;
 	dog->owner = _strcpy(dog->owner, owner);
 	return (dog);
 }
