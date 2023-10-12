aadd7a3a3fc176fcb8659162a45fdb0121524d8a
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-11 17:45:56 -0700

    Options operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two integers.
+ * @a: store integer value.
+ * @b: store integer value.
+ *
+ * Return: results to the main.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers.
+ * @a: store integer value.
+ * @b: store integer value.
+ *
+ * Return: results to the main.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers.
+ * @a: store integer value.
+ * @b: store integer value.
+ *
+ * Return: results to the main.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two integers.
+ * @a: store integer value.
+ * @b: store integer value.
+ *
+ *  Return: results to the main.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulas two integers.
+ * @a: store integer value.
+ * @b: store integer value.
+ *
+ * Return: results to the main.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
