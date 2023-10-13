c580b305c82bcf4bb67195d2cc9e8032662a9d48
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-12 17:35:25 -0700

    Print string with new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print string passed as argument.
+ * @separator: character string.
+ * @n: number of argument passed.
+ *
+ * Return: void.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j;
+	char *s;
+
+	va_list ag;
+
+	va_start(ag, n);
+
+	for (j = 0; j < n; j++)
+	{
+		s = va_arg(ag, char *);
+		if (!s)
+			s = "nil";
+		if (!separator)
+			printf("%s", s);
+		else if (separator && j == 0)
+			printf("%s", s);
+		else
+			printf("%s%s", separator, s);
+	}
+	va_end(ag);
+	printf("\n");
+}
