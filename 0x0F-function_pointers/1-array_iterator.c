fcbf1e7752e969c574fa5bfc853f140ff6a7c644
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-10-13 12:45:28 -0700

    Excutes parameters on each elements of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-	for (j = 0; j <= size - 1; j++)
+	for (j = 0; j < size; j++)
 	{
 		action(array[j]);
 	}
